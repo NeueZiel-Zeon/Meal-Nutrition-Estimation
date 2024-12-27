a5ce9b58bb2e2a9349c8f5e89822fb67719c9c2c
DESKTOP-OQI4795\owner <yuuki24682@gmail.com>
2024-12-28 00:25:43 +0900

    デプロイ用

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, "encoding"];
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
